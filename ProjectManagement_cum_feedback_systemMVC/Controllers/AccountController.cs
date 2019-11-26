16c43dd9ec3586428113b28ec8f90d55f9e20fab
swarpatel23 <smp1613@gmail.com>
2019-11-26 11:37:20 +0530

    login bug solved some css changed

--- ProjectManagement_cum_feedback_systemMVC/Controllers/AccountController.cs
+++ ProjectManagement_cum_feedback_systemMVC/Controllers/AccountController.cs
@@ -73,9 +73,26 @@ namespace ProjectManagement_cum_feedback_systemMVC.Controllers
                 return View(model);
             }
 
+            var result = SignInStatus.Failure;
+
+            try
+            {
+                ApplicationUserManager au = Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                var user = au.FindByEmail(model.Email);// where db is ApplicationDbContext instance
+                if (user != null)
+                {
+                    result = await SignInManager.PasswordSignInAsync(user.UserName, model.Password, model.RememberMe,
+                        shouldLockout: false);
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                // the user is not exist
+            }
+
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            //var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
             //Session["username"] = User.Identity.Name;
             switch (result)
             {
